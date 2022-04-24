e07e7743b1f44ae5077c89068e2434b4747408d8
Venzele <Venzele@yandex.ru>
2022-04-24 14:33:06 +0300

    CleanCode4

--- App2/Program.cs
+++ App2/Program.cs
@@ -8,14 +8,8 @@ namespace App2
 {
     class Program
     {
-        class Weapon
-        {
-            private int _bullets;
-            private int _bulletsPerShot;
-
-            public bool CanShoot() => _bullets >= _bulletsPerShot && _bulletsPerShot > 0;
-
-            public void Shoot() => _bullets -= _bulletsPerShot;
-        }
+        int armySize = 10;
+        int coins = 10;
+        string name = "Vladislav";
     }
 }
