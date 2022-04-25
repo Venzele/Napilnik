6d43b7bdd4f371faea60373b6da4bd5142fca6cf
Venzele <Venzele@yandex.ru>
2022-04-25 22:56:08 +0300

    CleanCode11

--- App2/Program.cs
+++ App2/Program.cs
@@ -8,101 +8,16 @@ namespace App2
 {
     class Program
     {
-        static void Main(string[] args)
+        public void Enable()
         {
-            OrderForm orderForm = new OrderForm();
-            string systemId = orderForm.ShowForm();
-
-            PaySystem paySystem = new PaySystem(systemId);
-            PaymentHandler paymentHandler = new PaymentHandler(paySystem);
-
-            paySystem.ShowPaymentPay();
-            paymentHandler.ShowPaymentResult(systemId);
-        }
-    }
-
-    public enum PaymentType
-    {
-        Qiwi,
-        WebMoney,
-        Card
-    }
-
-    public class OrderForm
-    {
-        public string ShowForm()
-        {
-            Console.WriteLine("Мы принимаем: QIWI, WebMoney, Card");
-
-            //симуляция веб интерфейса
-            Console.WriteLine("Какое системой вы хотите совершить оплату?");
-            return Console.ReadLine().ToLower();
-        }
-    }
-
-    public class PaymentHandler
-    {
-        private PaySystem _paySystem;
-
-        public PaymentHandler(PaySystem paySystem)
-        {
-            _paySystem = paySystem;
+            _enable = true;
+            _effects.StartEnableAnimation();
         }
 
-        public void ShowPaymentResult(string systemId)
+        public void Disabled()
         {
-            Console.WriteLine($"Вы оплатили с помощью {systemId}");
-            _paySystem.ShowPaymentResult();
-            Console.WriteLine("Оплата прошла успешно!");
+            _enable = false;
+            _pool.Free(this);
         }
-    }
-
-    public class PaySystem
-    {
-        private string _systemId;
-
-        public PaySystem(string systemId)
-        {
-            _systemId = systemId;
-        }
-
-        public void ShowPaymentPay()
-        {
-            Console.WriteLine($"{GetMassage()} {_systemId}...");
-        }
-
-        public void ShowPaymentResult()
-        {
-            Console.WriteLine($"Проверка платежа через {_systemId}...");
-        }
-
-        private string GetMassage()
-        {
-            if (_systemId == PaymentType.Qiwi.ToString().ToLower())
-                return "Перевод на страницу";
-
-            if (_systemId == PaymentType.WebMoney.ToString().ToLower())
-                return "Вызов API";
-
-            if (_systemId == PaymentType.Card.ToString().ToLower())
-                return "Вызов API банка эмитера карты";
-
-            throw new ArgumentOutOfRangeException(nameof(_systemId));
-        }
-    }
-
-    public class Qiwi : PaySystem
-    {
-        public Qiwi(string systemId) : base(systemId) { }
-    }
-
-    public class WebMoney : PaySystem
-    {
-        public WebMoney(string systemId) : base(systemId) { }
-    }
-
-    public class Card : PaySystem
-    {
-        public Card(string systemId) : base(systemId) { }
     }
 }
